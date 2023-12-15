994ffaa90fff85c25bc74d21a01d3ac04f92c865
AiDinaAgustin <aidinagustin2@gmail.com>
2023-12-15 18:52:19 +0700

    Add POST endpoint for creating a new Mahasiswa

--- controllers/mahasiswaController.js
+++ controllers/mahasiswaController.js
@@ -29,6 +29,19 @@ router.get('/:nim', (req, res) => {
     });
 });
 
+// POST /mahasiswa
+router.post('/', (req, res) => {
+    const { nim, nama, gender, prodi, alamat } = req.body;
+    db.query('INSERT INTO mahasiswa (nim, nama, gender, prodi, alamat) VALUES (?, ?, ?, ?, ?)', [nim, nama, gender, prodi, alamat], (error) => {
+        if (error) {
+            console.error('Error creating mahasiswa:', error);
+            res.status(500).json({ message: 'Internal Server Error' });
+        } else {
+            res.json({ message: 'Mahasiswa created successfully' });
+        }
+    });
+});
+
 //PUT /mahasiswa/:nim
 router.put('/:nim', (req, res) => {
     const mahasiswaNim = req.params.nim;
@@ -53,19 +66,6 @@ router.delete('/:nim', (req, res) => {
             res.status(500).json({ message: 'Internal Server Error' });
         } else {
             res.json({ message: 'Mahasiswa deleted successfully' });
-        }
-    });
-});
-
-// POST /mahasiswa
-router.post('/', (req, res) => {
-    const { nim, nama, gender, prodi, alamat } = req.body;
-    db.query('INSERT INTO mahasiswa (nim, nama, gender, prodi, alamat) VALUES (?, ?, ?, ?, ?)', [nim, nama, gender, prodi, alamat], (error) => {
-        if (error) {
-            console.error('Error creating mahasiswa:', error);
-            res.status(500).json({ message: 'Internal Server Error' });
-        } else {
-            res.json({ message: 'Mahasiswa created successfully' });
         }
     });
 });
